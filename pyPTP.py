2b84f012ba3d95b97c9008765f02522c32d0edae
Nate Muth <nmuth87@gmail.com>
2018-11-27 20:39:00 -0600

    Updated so that a count isn't required

--- pyPTP.py
+++ pyPTP.py
@@ -5,18 +5,27 @@ from numpy import random
 
 class process_time_printer():
     
-    def __init__(self, count):
-        self.count = count
+    def __init__(self, count=None):
+        if count is not None:
+            self.count = count
+        else:
+            self.count = False
+
         self.iteration = 1
         self.time_list = []
         self.start_time = time.time()
         
     def increment(self, st):
         self.time_list.append(self.get_time() - st)
-        print('| {0}/{1} complete | {2} seconds elapsed | {3} estimated seconds remaining |     '.format(
-            self.iteration, self.count, round(self.get_time() - self.start_time, 4)
-            , round((self.count - self.iteration) * np.mean(self.time_list), 4)
-        ), end='\r', flush=True)
+        if self.count == False:
+            print('| {0}/{1} complete | {2} seconds elapsed |     '.format(
+                self.iteration, self.count, round(self.get_time() - self.start_time, 4)
+            ), end='\r', flush=True)
+        else:
+            print('| {0}/{1} complete | {2} seconds elapsed | {3} estimated seconds remaining |     '.format(
+                self.iteration, self.count, round(self.get_time() - self.start_time, 4)
+                , round((self.count - self.iteration) * np.mean(self.time_list), 4)
+            ), end='\r', flush=True)
         self.iteration += 1
         
     def get_time(self):
